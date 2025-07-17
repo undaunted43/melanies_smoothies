0e820acaeae25558f31d6a8c1bf0f03443266970
undaunted43 <135414175+undaunted43@users.noreply.github.com>
2025-07-17 10:38:26 +0300

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -42,5 +41,6 @@ if ingredient_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
+cnx = st.connection("snowflake")
+session = cnx.session()
 
