b68fc0806ce876bae09f1f68e48d7614774123ac
undaunted43 <135414175+undaunted43@users.noreply.github.com>
2025-07-28 12:07:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ try:
     ingredient_list = st.multiselect("Choose up to five ingredients:", fruit_list, max_selections=5)
 
     smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response)
+    st.text(smoothiefroot_response.json())
 
     if ingredient_list:
         st.write("Selected ingredients:", ingredient_list)
